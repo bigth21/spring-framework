9295215b8bdeaeea4cad6c1fb1b7477707c5371a
bigth21 <taekhyeon.nam@gmail.com>
2025-03-30 17:59:14 +0900

    Add IoC Container-related

---
+++ src/main/java/me/bigth/springframework/IoCContainer.java
@@ -0,0 +1,26 @@
+package me.bigth.springframework;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+public class IoCContainer {
+
+    public static void main(String[] args) {
+        var context = new AnnotationConfigApplicationContext(AppConfig.class);
+        MyService service = context.getBean(MyService.class);
+        service.doSomething();
+    }
+
+    static class AppConfig {
+        @Bean
+        static MyService myService() {
+            return new MyService();
+        }
+    }
+
+    static class MyService {
+        public void doSomething() {
+            System.out.println("Do something");
+        }
+    }
+}
